bb6bbeb9d203a4252c8e9240c41ddbc2b88c7e03
Glebson Santos <glebsonsantos@outlook.pt>
2024-11-21 16:12:00 -0300

    init: Create BackEnd Project

---
+++ BackEnd/src/main/java/com/glebson/tierListMovies/TierListMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.glebson.tierListMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierListMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierListMoviesApplication.class, args);
+	}
+
+}
